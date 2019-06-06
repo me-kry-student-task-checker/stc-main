288f7c1b858c187f7a94d235bc6daf1fbe6e1e69
szattila98 <clownface68@gmail.com>
2019-06-06 14:02:03 +0200

    Added some clarification to the setup sh file.

--- backend/setup_docker.sh
+++ backend/setup_docker.sh
@@ -6,7 +6,8 @@ NOCOLOR='\033[0m'
 
 # Variables
 CONFIG_SERVICE="config-service"
-DISCOVERY_SERVICE="discovery-service"
+DISCOVERY_SERVICE_PEER1="discovery-service-peer1"
+DISCOVERY_SERVICE_PEER2="discovery-service-peer2"
 
 NETWORK_MASK="172.18.0.22/16"
 NETWORK_NAME="studentTaskChecker"
@@ -18,17 +19,17 @@ NETWORK_NAME="studentTaskChecker"
 echo -e "\n${YELLOW}Image deletion${NOCOLOR} \n"
 
 docker image rm -f ${CONFIG_SERVICE}
-docker image rm -f "${DISCOVERY_SERVICE}-peer1"
-docker image rm -f "${DISCOVERY_SERVICE}-peer2"
+docker image rm -f ${DISCOVERY_SERVICE_PEER1}
+docker image rm -f ${DISCOVERY_SERVICE_PEER2}
 
 echo -e "\n${YELLOW}Container deletion${NOCOLOR} \n"
 
 docker container stop ${CONFIG_SERVICE}
 docker container rm ${CONFIG_SERVICE}
-docker container stop "${DISCOVERY_SERVICE}-peer1"
-docker container rm "${DISCOVERY_SERVICE}-peer1"
-docker container stop "${DISCOVERY_SERVICE}-peer2"
-docker container rm "${DISCOVERY_SERVICE}-peer2"
+docker container stop ${DISCOVERY_SERVICE_PEER1}
+docker container rm ${DISCOVERY_SERVICE_PEER1}
+docker container stop ${DISCOVERY_SERVICE_PEER2}
+docker container rm ${DISCOVERY_SERVICE_PEER2}
 
 echo -e "\n${YELLOW}Building images from docker files${NOCOLOR} \n"
 
@@ -36,8 +37,8 @@ cd config-service
 docker build -f dockerfile -t ${CONFIG_SERVICE} .
 cd ..
 cd discovery-service
-docker build -f dockerfile-p1 -t "${DISCOVERY_SERVICE}-peer1" .
-docker build -f dockerfile-p2 -t "${DISCOVERY_SERVICE}-peer2" .
+docker build -f dockerfile-p1 -t ${DISCOVERY_SERVICE_PEER1} .
+docker build -f dockerfile-p2 -t ${DISCOVERY_SERVICE_PEER2} .
 cd ..
 
 echo -e "\n${YELLOW}Deleting existing network and creating it anew${NOCOLOR} \n"
@@ -48,5 +49,5 @@ docker network create --subnet=${NETWORK_MASK} ${NETWORK_NAME}
 echo -e "\n${YELLOW}Running docker images${NOCOLOR} \n"
 
 docker run --detach --name ${CONFIG_SERVICE} --net ${NETWORK_NAME} -it ${CONFIG_SERVICE}
-docker run --detach --name "${DISCOVERY_SERVICE}-peer1" --net ${NETWORK_NAME} -it "${DISCOVERY_SERVICE}-peer1"
-docker run --detach --name "${DISCOVERY_SERVICE}-peer2" --net ${NETWORK_NAME} -it "${DISCOVERY_SERVICE}-peer2"
+docker run --detach --name ${DISCOVERY_SERVICE_PEER1} --net ${NETWORK_NAME} -it ${DISCOVERY_SERVICE_PEER1}
+docker run --detach --name ${DISCOVERY_SERVICE_PEER2} --net ${NETWORK_NAME} -it ${DISCOVERY_SERVICE_PEER2}
