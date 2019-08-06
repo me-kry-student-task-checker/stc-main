f7300b46f3e79d3a62e62e37d4a535e7cf723035
szattila98 <clownface68@gmail.com>
2019-08-06 20:17:48 +0200

    Changed course service port to 8080, it's in a different docker container so it shouldn't be on another port, it can confuse easily if it is. Pom fixes. Added swagger2 to course-service for future use. Added eureka client config.

--- backend/course-service/src/main/java/hu/me/iit/malus/thesis/course/CourseServiceApplication.java
+++ backend/course-service/src/main/java/hu/me/iit/malus/thesis/course/CourseServiceApplication.java
@@ -2,13 +2,24 @@ package hu.me.iit.malus.thesis.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Course Microservice
  *
  * @author Javorek Dénes
+ * @author Szőke Attila
  */
 @SpringBootApplication
+@EnableEurekaClient
+@EnableSwagger2
 public class CourseServiceApplication {
 
 	/**
@@ -20,4 +31,18 @@ public class CourseServiceApplication {
 		SpringApplication.run(CourseServiceApplication.class, args);
 	}
 
+	/**
+	 * Swagger config bean.
+	 *
+	 * @return the api docket
+	 */
+	@Bean
+	public Docket swaggerApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("hu.me.iit.malus.thesis.course"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(new ApiInfoBuilder().version("1.0").title("Course-service API").description("Course service API v1.0").build());
+	}
 }
