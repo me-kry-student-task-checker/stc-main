d3faa1f7a8048463ee6ea83d360f4c0ee8b1421c
Javorek, Denes <javorek.denes@gmail.com>
2019-08-07 20:31:07 +0200

    Cloud SQL is connected to course-service successfully. Service key is uploaded to the repo!

--- backend/course-service/src/main/java/hu/me/iit/malus/thesis/course/repository/CourseRepository.java
+++ backend/course-service/src/main/java/hu/me/iit/malus/thesis/course/repository/CourseRepository.java
@@ -3,5 +3,9 @@ package hu.me.iit.malus.thesis.course.repository;
 import hu.me.iit.malus.thesis.course.model.Course;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ *
+ * @author Javorek Dénes
+ */
 public interface CourseRepository extends CrudRepository<Course, Long> {
 }
