641686774984e2f5bcb46f475ebeb1046c9735ad
Javorek, Denes <javorek.denes@gmail.com>
2019-08-12 13:56:23 +0200

    Added mysql container to docker, created different maven profiles, created the required db init script

---
+++ backend/docker/provision/mysql/init/01_databases.sql
@@ -0,0 +1,6 @@
+# Each database for different services has to be defined here
+CREATE DATABASE IF NOT EXISTS `courses`;
+
+# create root user and grant rights
+CREATE USER 'courseservice'@'localhost' IDENTIFIED BY 'course';
+GRANT ALL PRIVILEGES ON courses.* TO 'courseservice'@'localhost';
