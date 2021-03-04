a538d35803f111995359c77d7d8339e9ed29f9f1
szattila98 <clownface68@gmail.com>
2021-03-04 15:12:24 +0100

    Final adattagok és RequiredArgsConstructor annotáció hozzáadva, logok kijavítva és ahol nem volt pótolva.

--- backend/course-service/src/main/java/hu/me/iit/malus/thesis/course/service/impl/CourseServiceImpl.java
+++ backend/course-service/src/main/java/hu/me/iit/malus/thesis/course/service/impl/CourseServiceImpl.java
@@ -1,13 +1,16 @@
 package hu.me.iit.malus.thesis.course.service.impl;
 
-import hu.me.iit.malus.thesis.course.client.*;
+import hu.me.iit.malus.thesis.course.client.FeedbackClient;
+import hu.me.iit.malus.thesis.course.client.FileManagementClient;
+import hu.me.iit.malus.thesis.course.client.TaskClient;
+import hu.me.iit.malus.thesis.course.client.UserClient;
 import hu.me.iit.malus.thesis.course.model.Course;
 import hu.me.iit.malus.thesis.course.model.exception.ForbiddenCourseEdit;
 import hu.me.iit.malus.thesis.course.repository.CourseRepository;
 import hu.me.iit.malus.thesis.course.service.CourseService;
 import hu.me.iit.malus.thesis.course.service.exception.CourseNotFoundException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -21,36 +24,15 @@ import java.util.Set;
  * @author Attila Szőke
  */
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class CourseServiceImpl implements CourseService {
 
-    private CourseRepository courseRepository;
-    private TaskClient taskClient;
-    private FeedbackClient feedbackClient;
-    private UserClient userClient;
-    private FileManagementClient fileManagementClient;
-    private EmailClient emailClient;
-
-    /**
-     * Instantiates a new Course service.
-     *
-     * @param courseRepository     the course repository
-     * @param taskClient           the task client
-     * @param feedbackClient       the feedback client
-     * @param userClient           the user client
-     * @param fileManagementClient the fileManagement client
-     * @param emailClient          the email client
-     */
-    @Autowired
-    public CourseServiceImpl(CourseRepository courseRepository, TaskClient taskClient, FeedbackClient feedbackClient, UserClient userClient,
-                             FileManagementClient fileManagementClient, EmailClient emailClient) {
-        this.courseRepository = courseRepository;
-        this.taskClient = taskClient;
-        this.feedbackClient = feedbackClient;
-        this.userClient = userClient;
-        this.fileManagementClient = fileManagementClient;
-        this.emailClient = emailClient;
-    }
+    private final CourseRepository courseRepository;
+    private final TaskClient taskClient;
+    private final FeedbackClient feedbackClient;
+    private final UserClient userClient;
+    private final FileManagementClient fileManagementClient;
 
     /**
      * {@inheritDoc}
@@ -58,9 +40,9 @@ public class CourseServiceImpl implements CourseService {
     @Override
     public Course create(Course course, String creatorsEmail) {
         course.setCreationDate(Date.from(Instant.now()));
-        Course newCourse = courseRepository.save(course);
-        userClient.saveCourseCreation(newCourse.getId());
-        log.info("Created course: {}", newCourse.getId());
+        Course newCourse = this.courseRepository.save(course);
+        this.userClient.saveCourseCreation(newCourse.getId());
+        log.debug("Created course: {}", newCourse.getId());
         return newCourse;
     }
 
@@ -69,14 +51,15 @@ public class CourseServiceImpl implements CourseService {
      */
     @Override
     public Course edit(Course course, String editorsEmail) {
-        Course oldCourse = courseRepository.getOne(course.getId());
+        Course oldCourse = this.courseRepository.getOne(course.getId());
 
         if (!oldCourse.getCreator().getEmail().equals(editorsEmail)) {
+            log.warn("Creator of this course {} is not the editor: {}!", course, editorsEmail);
             throw new ForbiddenCourseEdit();
         }
 
-        log.info("Modified course: {}", course.getId());
-        return courseRepository.save(course);
+        log.debug("Modified course: {}", course.getId());
+        return this.courseRepository.save(course);
     }
 
     /**
@@ -84,21 +67,22 @@ public class CourseServiceImpl implements CourseService {
      */
     @Override
     public Course get(Long courseId, String userEmail) throws CourseNotFoundException {
-        Optional<Course> optCourse = courseRepository.findById(courseId);
+        Optional<Course> optCourse = this.courseRepository.findById(courseId);
 
         if (optCourse.isPresent()) {
             Course course = optCourse.get();
-            if (!userClient.isRelated(course.getId())) {
+            if (!this.userClient.isRelated(course.getId())) {
+                log.warn("User {} is not realated to this course {}!", userEmail, course);
                 throw new CourseNotFoundException();
             }
 
             //TODO: We should find a way to fire these as async requests
-            course.setCreator(userClient.getTeacherByCreatedCourseId(courseId));
-            course.setStudents(userClient.getStudentsByAssignedCourseId(courseId));
-            course.setTasks(taskClient.getAllTasks(courseId));
-            course.setFiles(fileManagementClient.getAllFilesByTagId(hu.me.iit.malus.thesis.course.client.dto.Service.COURSE, courseId).getBody());
-            course.setComments(feedbackClient.getAllCourseComments(courseId));
-            log.info("Course found: {}", courseId);
+            course.setCreator(this.userClient.getTeacherByCreatedCourseId(courseId));
+            course.setStudents(this.userClient.getStudentsByAssignedCourseId(courseId));
+            course.setTasks(this.taskClient.getAllTasks(courseId));
+            course.setFiles(this.fileManagementClient.getAllFilesByTagId(hu.me.iit.malus.thesis.course.client.dto.Service.COURSE, courseId).getBody());
+            course.setComments(this.feedbackClient.getAllCourseComments(courseId));
+            log.debug("Course found: {}", courseId);
             return course;
         } else {
             log.warn("No course found with this id: {}", courseId);
@@ -111,13 +95,13 @@ public class CourseServiceImpl implements CourseService {
      */
     @Override
     public Set<Course> getAll(String userEmail) {
-        Set<Long> relatedCourseIds = userClient.getRelatedCourseIds();
-        Set<Course> relatedCourses = courseRepository.findAllByIdIsIn(relatedCourseIds);
+        Set<Long> relatedCourseIds = this.userClient.getRelatedCourseIds();
+        Set<Course> relatedCourses = this.courseRepository.findAllByIdIsIn(relatedCourseIds);
 
         for (Course course : relatedCourses) {
-            course.setCreator(userClient.getTeacherByCreatedCourseId(course.getId()));
+            course.setCreator(this.userClient.getTeacherByCreatedCourseId(course.getId()));
         }
-        log.info("Get all courses done, total number of courses is {}", relatedCourses.size());
+        log.debug("Get all courses done, total number of courses is {}", relatedCourses.size());
         return relatedCourses;
     }
 }
